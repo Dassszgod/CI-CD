746c481a7263a1896f25e082e173c83d57b9b45c
Bogdan Tarynskyi <tarynskyi.bohdan@gmail.com>
2025-03-26 17:23:47 +0200

    Додано функцію повернення зміни ціни за місяць

--- main.py
+++ main.py
@@ -21,12 +21,34 @@ def read_product_data(file_path):
                 continue 
     return products
 
-def filter_last_month(products):
-    today = datetime.date.today()
-    last_month = today - datetime.timedelta(days=30)
-    return [p for p in products if p[1] >= last_month]
+def get_last_two_prices(products, product_name):
+    filtered = [p for p in products if p[0] == product_name]
+    if len(filtered) < 2:
+        print(f"Недостатньо даних для {product_name}")
+        return None
+
+    filtered.sort(key=lambda x: x[1]) 
+    return filtered[-2:]  
+
+
+
+def price_change(products, product_name):
+    filtered = [p for p in products if p[0] == product_name]
+    if len(filtered) < 2:
+        return None
+
+    filtered.sort(key=lambda x: x[1])
+    return filtered[-1][2] - filtered[0][2]
+
 
 if __name__ == "__main__":
     data = read_product_data("data.txt")
-    last_month_data = filter_last_month(data)
-    print("Прочитані дані:", last_month_data)
+    print("Прочитані дані:", data)
+
+    last_two_prices = get_last_two_prices(data, "Apple")
+    print("Останні два записи про Apple:", last_two_prices)
+
+    if last_two_prices:
+        price_diff = last_two_prices[1][2] - last_two_prices[0][2]
+        print("Зміна ціни:", price_diff)
+
